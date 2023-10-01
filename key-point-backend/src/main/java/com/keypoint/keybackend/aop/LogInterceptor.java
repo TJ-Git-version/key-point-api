7595919db36678065ec0041aa9448402a595a9ac
key point <keyPoint.cn>
2023-10-02 01:06:25 +0800

    修改aop扫描包名

--- key-point-backend/src/main/java/com/keypoint/keybackend/aop/LogInterceptor.java
+++ key-point-backend/src/main/java/com/keypoint/keybackend/aop/LogInterceptor.java
@@ -27,7 +27,7 @@ public class LogInterceptor {
     /**
      * 执行拦截
      */
-    @Around("execution(* com.qimu.qiapibackend.controller.*.*(..))")
+    @Around("execution(* com.keypoint.keybackend.controller.*.*(..))")
     public Object doInterceptor(ProceedingJoinPoint point) throws Throwable {
         // 计时
         StopWatch stopWatch = new StopWatch();
